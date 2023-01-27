6692b59978cc23340bc3cf004320eb69680ad852
tomasprikasky <tom.prikasky@seznam.cz>
2023-01-27 21:35:24 +0100

    druhy insert

---
+++ Areas/SpravaZpravy/Controllers/OdesilaniController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Zad.WEB_1.Areas.SpravaZpravy.Models;
+
+namespace Zad.WEB_1.Areas.SpravaZpravy.Controllers
+{
+    [Area("SpravaZpravy")] //9. přidání reference na areu tak aby byl routing kompletní
+    public class OdesilaniController : Controller
+    {
+        public IActionResult Odeslani() //vytvořeni akcnich metod krok 1 - pouze deklarovani
+        {
+            Modesilani modesilani = new Modesilani();
+            
+            return View(modesilani); //uprava
+        }
+
+        public IActionResult UspesneOdeslani() //vytvořeni akcnich metod krok 1 - pouze deklarovani
+        {
+            return View();
+        }
+
+
+        [HttpPost] // 8 pridání validace na straně serveru
+        public IActionResult Odeslani(Modesilani modesilani)
+        {
+
+            if (ModelState.IsValid)
+            {
+
+                return RedirectToAction(nameof(OdesilaniController.UspesneOdeslani));// 8. přesměrování na příslušný wiev
+
+            }
+
+            return View(modesilani);
+
+
+        }
+    }
+
+}
---
+++ Areas/SpravaZpravy/Models/Modesilani.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Zad.WEB_1.Areas.SpravaZpravy.Models
+{
+    public class Modesilani
+    {
+        [Display(Name = "Zadejte odesilatele")]//7 nastaveni validace dle pozadavku
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Odesilatel musi mit minimalne 3 a maximalne 30 znaku")]
+        [Required]
+        public string Odesilatel { get; set; } = ""; // krok 4 zadefinovani promenych
+        
+        
+        [Display(Name = "Zadejte příjemce")]//7 nastaveni validace dle pozadavku
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Odesilatel musi mit minimalne 3 a maximalne 30 znaku")]
+        [Required]
+        public string Nazev { get; set; } = ""; // krok 4 zadefinovani promenych
+
+
+        [Display(Name = "Zadejte zpravu")]
+        [Required]//7 nastaveni validace dle pozadavku
+        public string Zprava { get; set; } = "";    // krok 4 zadefinovani promenych
+
+        public Modesilani()                     // kork 4 zadefinovani nove instance tridy
+        {
+
+        }
+    }
+}
